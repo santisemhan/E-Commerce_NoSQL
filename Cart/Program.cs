0f74690f87f57dba73c6e3f1efe6c374e8e31d80
Santiago Semhan <santisemhan2@gmail.com>
2022-11-01 15:32:25 -0300

    Add Transient

--- Cart/Program.cs
+++ Cart/Program.cs
@@ -1,5 +1,9 @@
+using Cart.Core.Contexts;
 using Cart.Core.Helpers.Middleware;
+using Cart.Core.Repositories.Contexts;
+using Cart.Core.Repositories.Contexts.Interfaces;
 using Microsoft.AspNetCore.Localization;
+using Redis.OM;
 using System.Globalization;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -24,6 +28,9 @@ services.Configure<RequestLocalizationOptions>(opts => {
     // UI strings that we have localized.
     opts.SupportedUICultures = supportedCultures;
 });
+
+services.AddTransient<IConnection<RedisConnectionProvider>, RedisDataContext>();
+services.AddTransient<IConnection<Cassandra.ISession>, CassandraDataContext>();
 
 var app = builder.Build();
 
