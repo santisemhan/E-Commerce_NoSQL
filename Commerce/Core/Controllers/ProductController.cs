a4884eb07a093665005abb9e64002ca1a2d3b0cf
Lifeliine <matias.cespedes@outlook.com>
2022-11-10 12:58:24 -0300

    controladores para usuario y producto

---
+++ Commerce/Core/Controllers/ProductController.cs
@@ -0,0 +1,56 @@
+﻿using Commerce.Core.DataTransferObjects;
+using Commerce.Core.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Commerce.Core.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProductController : ControllerBase
+{
+    private readonly IProductService productService;
+
+    public ProductController(IProductService productService)
+    {
+        this.productService = productService;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllProducts()
+    {
+        return Ok(await productService.GetAllProducts());
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetProductById(string id)
+    {
+        return Ok(await productService.GetProductById(id));
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateProduct([FromBody] ProductDTO product)
+    {
+        if (product == null) { return BadRequest(); }
+
+        await productService.InsertProduct(product);
+
+        return Created("Created", true);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> UpdateProduct([FromBody] ProductDTO product, string id)
+    {
+        if (product == null) { return BadRequest(); }
+
+        await productService.UpdateProduct(product, id);
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteProduct(string id)
+    {
+        await productService.DeleteProduct(id);
+        return NoContent();
+    }
+}
---
+++ Commerce/Core/Controllers/UserController.cs
@@ -0,0 +1,57 @@
+﻿using Cart.Core.DataTransferObjects;
+using Commerce.Core.Services.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Commerce.Core.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UserController : ControllerBase
+{
+    private readonly IUserService userService;
+
+    public UserController(IUserService userService)
+    {
+        this.userService = userService;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllUsers()
+    {
+        return Ok(await userService.GetAllUsers());
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetUserById(string id)
+    {
+        return Ok(await userService.GetUserById(id));
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateUser([FromBody] UserDTO user)
+    {
+        if (user == null) { return BadRequest(); }
+
+        await userService.InsertUser(user);
+
+        return Created("Created", true);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> UpdateUser([FromBody] UserDTO user, string id)
+    {
+        if (user == null) { return BadRequest(); }
+
+        await userService.UpdateUser(user, id);
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteUser(string id)
+    {
+        await userService.DeleteUser(id);
+        return NoContent();
+    }
+}
